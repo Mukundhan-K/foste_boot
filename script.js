1f04c6d8a6cd320bf7ef39260daa5682875b5e41
Mukundhan-K <140101554+Mukundhan-K@users.noreply.github.com>
2023-10-07 16:52:39 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+let navv = document.getElementById('navbar_cont')
+
+let menu =document.getElementById('menubar')
+
+navv.onclick = () => {
+    navv.style.display='none';
+}
+
+menu.onclick = () => {
+    navv.style.display='grid';
+
+}
